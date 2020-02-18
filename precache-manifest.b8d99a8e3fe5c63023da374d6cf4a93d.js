1f8800f343c48a459ec48f26a930fa45c8871dda
Adam Kret <adamkret93@gmail.com>
2020-02-18 11:21:58 +0100

    Updates

---
+++ precache-manifest.b8d99a8e3fe5c63023da374d6cf4a93d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf959ffdfc83632e35392ffde9c404d8",
+    "url": "/layout-editor/index.html"
+  },
+  {
+    "revision": "d15d28dda0e45f508ab4",
+    "url": "/layout-editor/static/css/main.520cc941.chunk.css"
+  },
+  {
+    "revision": "eb6dc975856873229895",
+    "url": "/layout-editor/static/js/2.eb399b1c.chunk.js"
+  },
+  {
+    "revision": "546786d89953f1382d863f2b9089951f",
+    "url": "/layout-editor/static/js/2.eb399b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15d28dda0e45f508ab4",
+    "url": "/layout-editor/static/js/main.38a6a1f1.chunk.js"
+  },
+  {
+    "revision": "6a301c97660bf1aa59c9",
+    "url": "/layout-editor/static/js/runtime-main.e177a67a.js"
+  },
+  {
+    "revision": "528142350b3c0e7556d1bf5ddcf726e5",
+    "url": "/layout-editor/static/media/happy.52814235.svg"
+  },
+  {
+    "revision": "13fa19126be9a2a178a2a259ca3fc3eb",
+    "url": "/layout-editor/static/media/loader.13fa1912.gif"
+  },
+  {
+    "revision": "473d3844e933a0bda7b38f53154677c6",
+    "url": "/layout-editor/static/media/sad.473d3844.svg"
+  }
+]);
